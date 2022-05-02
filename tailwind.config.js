41087a709ae6a5ada64a4df1aae1e60d5c70a99f
meow0516 <76482269+meow0516@users.noreply.github.com>
2022-05-02 14:58:27 +0800

    chore: add extend colors in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#E6EBF4',
+          200: '#A2B7CE',
+          400: '#436E9E',
+          500: '#165698',
+          600: '#053461',
+        },
+        secondary: {
+          100: '#FDE5F2',
+          200: '#F4B8D9',
+          400: '#DB579F',
+          500: '#E3238B',
+          600: '#AF0E65',
+        },
+      },
+    },
   },
   plugins: [],
 };
