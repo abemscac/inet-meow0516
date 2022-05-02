6b22bc951c9689e50501e65be3648c75c2f2c871
meow0516 <76482269+meow0516@users.noreply.github.com>
2022-05-02 23:35:21 +0800

    feat: add nav for guests(mobile)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      sm: { max: '680px' },
+    },
     extend: {
       colors: {
         primary: {
