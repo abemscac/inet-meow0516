ae6d1cc6a37d439f4a87c585554ef489f85caadb
meow0516 <76482269+meow0516@users.noreply.github.com>
2022-05-02 15:04:42 +0800

    feat: add nav for guests(desktop)

---
+++ src/components/common-components/NavBar.vue
@@ -0,0 +1,198 @@
+<template>
+  <nav class="flex justify-between px-16 py-3">
+    <img src="../../assets/logo.svg" alt="inet" />
+    <div class="flex w-1/3 rounded border border-primary-200 px-2">
+      <input class="w-11/12" type="text" placeholder="搜尋INET" />
+      <button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 text-primary-200"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+      </button>
+    </div>
+    <div v-if="!isLogin">
+      <button
+        class="rounded-full border border-primary-500 text-primary-500 px-8 py-2.5 mr-4"
+        @click="login"
+      >
+        登入
+      </button>
+      <button class="rounded-full bg-primary-500 text-white px-8 py-2.5">
+        註冊
+      </button>
+    </div>
+    <div v-else class="flex">
+      <button class="mr-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+          />
+        </svg>
+      </button>
+      <button @click="showDropdownMenu">
+        <img
+          src="../../assets/default-avatar 1.png"
+          alt="avatar"
+          class="avatar"
+        />
+      </button>
+      <div
+        v-show="menuVisible"
+        class="z-10 absolute py-4 overlayMenu"
+        ref="overlayMenu"
+      >
+        <ul class="flex flex-col items-start">
+          <li class="py-1">
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 inline-block mx-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                />
+              </svg>
+              <span>個人頁面</span>
+            </button>
+          </li>
+          <li class="py-1">
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 inline-block mx-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+                />
+              </svg>
+              <span>我的收藏</span>
+            </button>
+          </li>
+          <li class="py-1">
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 inline-block mx-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <span>瀏覽紀錄</span>
+            </button>
+          </li>
+          <li class="py-1">
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 inline-block mx-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+              </svg>
+              <span>帳號設定</span>
+            </button>
+          </li>
+          <li class="py-1">
+            <button @click="logout">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 inline-block mx-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                />
+              </svg>
+              <span>登出</span>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isLogin = ref(false);
+
+const login = () => {
+  isLogin.value = true;
+};
+
+const logout = () => {
+  isLogin.value = false;
+  menuVisible.value = false;
+};
+
+const menuVisible = ref(false);
+const showDropdownMenu = (e: Event) => {
+  menuVisible.value = !menuVisible.value;
+};
+</script>
+
+<style scoped lang="scss">
+.avatar {
+  height: 40px;
+}
+
+.overlayMenu {
+  top: 40px;
+}
+</style>
