aa141371e4361fc847390fc6635d4b3cbb66a9d7
meow0516 <76482269+meow0516@users.noreply.github.com>
2022-05-16 17:17:19 +0800

    chore: setup fontawesome

---
+++ src/Utilities/fontawesome-icons.ts
@@ -0,0 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faArrowRightFromBracket,
+  faArrowRightToBracket,
+  faBars,
+  faGear,
+  faMagnifyingGlass,
+  faUserPlus,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faBookmark,
+  faClock,
+  faPenToSquare,
+  faUser,
+} from '@fortawesome/free-regular-svg-icons';
+
+library.add(
+  faArrowRightFromBracket,
+  faArrowRightToBracket,
+  faBars,
+  faBookmark,
+  faClock,
+  faGear,
+  faMagnifyingGlass,
+  faPenToSquare,
+  faUser,
+  faUserPlus
+);
+
+export default FontAwesomeIcon;
