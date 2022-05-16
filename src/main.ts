6cf0645465a78f3ac93a5d63b3bf77d2245c4b62
meow0516 <76482269+meow0516@users.noreply.github.com>
2022-05-16 17:17:19 +0800

    chore: setup fontawesome

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css';
 import store from './vuex/Store';
+import FontAwesomeIcon from '../src/Utilities/fontawesome-icons';
 
-createApp(App).use(store).mount('#app');
+createApp(App)
+  .use(store)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
