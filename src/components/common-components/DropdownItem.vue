1d1602ee1e0843a774eed949e8b0fca631d6ac40
meow0516 <76482269+meow0516@users.noreply.github.com>
2022-05-29 23:38:21 +0800

    feat: add common-component DropdownContainer, DropdownItem

---
+++ src/components/common-components/DropdownItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <li class="py-1">
+    <button>
+      <font-awesome-icon :icon="icon" class="w-4 mx-2" />
+      <span>
+        <slot>{{ label }}</slot>
+      </span>
+    </button>
+  </li>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from 'vue';
+
+type Props = {
+  icon: string | Array<string>;
+  label?: string;
+};
+const props = defineProps<Props>();
+const { icon, label } = toRefs(props);
+</script>
