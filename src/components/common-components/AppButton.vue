e18e4f36f4b462f9e9f15f60a08003378d1d79c0
meow0516 <76482269+meow0516@users.noreply.github.com>
2022-06-19 22:47:58 +0800

    feat: add common-component - AppButton

---
+++ src/components/common-components/AppButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="rounded-full px-8 py-2">
+    <slot> {{ label }}</slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from 'vue';
+
+type Props = {
+  label: string;
+};
+const props = defineProps<Props>();
+const { label } = toRefs(props);
+</script>
