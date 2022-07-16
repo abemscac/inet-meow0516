f799ace082cbcd37ef7dca7378af7deecefc18c2
meow0516 <76482269+meow0516@users.noreply.github.com>
2022-07-16 16:24:42 +0800

    feat: add RegisterPage

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <Sidebar v-if="store.state.sidebarModule.visible" />
-  <div>
-    <NavBar />
+  <div class="h-screen bg-base sm:bg-white">
+    <NavBar v-if="!isRegisterPage" />
     <main class="w-full" @click="closeUserboxMenu">
       <router-view></router-view>
     </main>
@@ -9,14 +9,22 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+import { useRoute } from 'vue-router';
+import { RoutePath } from './router';
 import NavBar from './components/common-components/NavBar.vue';
 import Sidebar from './components/common-components/AppSidebar.vue';
-import { useStore } from 'vuex';
 
 const store = useStore();
+const route = useRoute();
 const closeUserboxMenu = () => {
   store.dispatch('userboxModule/close');
 };
+
+const isRegisterPage = computed(() =>
+  route.path === RoutePath.register ? true : false
+);
 </script>
 
 <style>
--- src/components/pages/RegisterPage.vue
+++ src/components/pages/RegisterPage.vue
@@ -1,1 +1,86 @@
-<template>This is register</template>
+<template>
+  <div class="flex justify-center h-full">
+    <div class="flex flex-col items-center w-4/12 sm:w-9/12 py-10">
+      <img src="../../assets/logo-I-rounded.png" alt="inet" class="w-16" />
+      <h1 class="text-3xl font-bold my-6">註冊 INET</h1>
+      <form>
+        <div class="mb-4">
+          <input
+            type="text"
+            class="w-full border border-primary-200 rounded-lg p-2 mb-1"
+            :class="isError ? 'border-indicator-danger' : ''"
+            placeholder="帳號*"
+          />
+          <p
+            v-if="isError"
+            class="text-left text-xs px-1 text-indicator-danger"
+          >
+            Error message(TBD)
+          </p>
+          <p class="text-left text-xs px-1 text-gray-400">
+            帳號長度需為4-30個字元，且為英文字母或數字開頭。<br />
+            可使用以下特殊符號 _.-
+          </p>
+        </div>
+        <div class="mb-4">
+          <input
+            type="text"
+            class="w-full border border-primary-200 rounded-lg p-2 mb-1"
+            :class="isError ? 'border-indicator-danger' : ''"
+            placeholder="密碼*"
+          />
+          <p
+            v-if="isError"
+            class="text-left text-xs px-1 text-indicator-danger"
+          >
+            Error message(TBD)
+          </p>
+          <p class="text-left text-xs px-1 text-gray-400">
+            密碼長度需為4-30個字元，且為英文字母或數字開頭。<br />可使用以下特殊符號
+            _.~!@#$%^&*()-+
+          </p>
+        </div>
+        <div class="mb-4">
+          <input
+            type="text"
+            class="w-full border border-primary-200 rounded-lg p-2 mb-1"
+            :class="isError ? 'border-indicator-danger' : ''"
+            placeholder="確認密碼*"
+          />
+          <p
+            v-if="isError"
+            class="text-left text-xs px-1 text-indicator-danger"
+          >
+            Error message(TBD)
+          </p>
+          <p class="text-left text-xs px-1 text-gray-400">請再次輸入密碼</p>
+        </div>
+        <AppButton label="註冊" class="w-full btn-primary-dark" />
+      </form>
+      <p class="my-2">
+        已經有帳號了嗎？<button class="text-primary-400" @click="navToLogin">
+          登入
+        </button>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useStore } from 'vuex';
+import router, { RoutePath } from '../../router';
+import AppButton from '../common-components/AppButton.vue';
+
+const store = useStore();
+if (store.state.userInfoModule.isLogin) {
+  router.push({ path: RoutePath.home });
+}
+
+const isError = ref(false);
+const navToLogin = () => {
+  console.log('router push');
+  // router.push()
+};
+</script>
+<style lang="scss"></style>
