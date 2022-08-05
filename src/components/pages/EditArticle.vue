f26637144533051321c8767316b441cbec4aa7bf
meow0516 <meow0516@gmail.com>
2022-08-05 15:01:56 +0800

    feat: add RWD class in EditArticle

--- src/components/pages/EditArticle.vue
+++ src/components/pages/EditArticle.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="w-1/3 md:w-full pt-10 sm:pt-2 flex flex-col m-auto">
-    <div class="w-full flex justify-start px-2 pb-2">
+  <div class="w-1/3 md:w-full md:pt-0 flex flex-col m-auto relative">
+    <div
+      class="w-full flex justify-start p-2 mt-4 sm:sticky sm:top-0 sm:bg-white"
+    >
       <AppButton
         label="撰寫"
         class="px-3 mr-3"
@@ -24,8 +26,11 @@
         @click="updateDisplayMode"
       />
     </div>
-    <section v-if="displayMode === 'edit'" class="p-8 bg-white">
-      <form class="flex flex-col gap-y-4">
+    <section
+      v-if="displayMode === 'edit'"
+      class="flex flex-col px-8 py-4 md:p-4 bg-white"
+    >
+      <form class="flex flex-col gap-y-4 input-area">
         <div class="flex">
           <select
             v-model="article.category"
@@ -49,29 +54,30 @@
           />
         </div>
         <div
-          class="border border-gray-300 border-dashed rounded-lg bg-gray-100"
+          class="border border-gray-300 border-dashed rounded-lg bg-gray-100 text-xs"
           :class="isInputImgActive ? 'border-4' : ''"
           @drop.prevent="onDrop"
           @dragenter.prevent="setInputImgActive"
           @dragover.prevent="setInputImgActive"
           @dragleave.prevent="setInputImgInactive"
         >
-          <div v-if="!previewImage" class="flex flex-col items-center h-40">
+          <div v-if="!previewImage" class="flex flex-col items-center h-44">
             <img src="../../assets/Icon.png" alt="upload" class="w-14 my-6" />
+            <p class="text-gray-300">拖曳圖片至此或</p>
             <AppButton
               label="上傳一張圖片"
               :icon="['fa', 'arrow-up-from-bracket']"
-              class="btn-primary-light"
+              class="btn-primary-light my-2"
               @click="handleInputClick"
             ></AppButton>
           </div>
-          <img v-else :src="previewImage" class="w-full h-40 object-cover" />
+          <img v-else :src="previewImage" class="w-full h-44 object-cover" />
         </div>
         <AppButton
           v-if="previewImage"
           label="更換圖片"
           :icon="['fa', 'image']"
-          class="btn-primary-dark w-40"
+          class="w-40 px-1 text-xs self-center btn-primary-dark"
           @click="handleInputClick"
         ></AppButton>
         <input
@@ -81,11 +87,9 @@
           accept="image/*"
           @change="handleChange"
         />
-
         <textarea
           v-model="article.body"
-          class="w-full border border-primary-200 rounded-lg p-4 overflow-y-auto"
-          :rows="previewImage ? 12 : 15"
+          class="w-full border border-primary-200 rounded-lg p-4 overflow-y-auto grow"
           placeholder="我覺得..."
           required
         />
@@ -94,22 +98,22 @@
       <AppButton
         label="發佈"
         :icon="['far', 'paper-plane']"
-        class="btn-primary-dark"
+        class="w-20 px-1 text-xs self-end btn-primary-dark"
       />
     </section>
-    <section v-else class="bg-white overflow-y-auto">
+    <section v-else class="bg-white">
       <article class="flex flex-col">
         <p class="p-6 font-bold text-left text-2xl">
           {{ article.title ?? '現在沒有標題' }}
         </p>
-        <div class="bg-gray-100 w-full h-80">
+        <div class="bg-gray-100 w-full h-80 sm:h-60">
           <img
             v-if="previewImage"
             :src="previewImage"
-            class="w-full h-80 object-cover"
+            class="w-full h-80 sm:h-60 object-cover"
           />
         </div>
-        <div class="p-6 text-left">
+        <div class="p-6 md:p-4 text-left">
           <div class="flex">
             <img src="../../assets/default-avatar_1.png" class="h-9" />
             <div class="flex flex-col items-start px-4">
@@ -134,7 +138,7 @@
               <p>也很多!</p>
             </div>
           </div>
-          <p class="py-4 overflow-y-auto h-60">
+          <p class="py-4 break-words">
             {{ article.body ?? '現在還沒有輸入內文' }}
           </p>
         </div>
@@ -254,3 +258,16 @@ const setInputImgInactive = () => {
   isInputImgActive.value = false;
 };
 </script>
+<style lang="scss" scoped>
+@media (min-width: 680px) {
+  .input-area {
+    height: calc(100vh - 300px);
+  }
+}
+
+@media (max-width: 680px) {
+  .input-area {
+    height: calc(100vh - 250px);
+  }
+}
+</style>
