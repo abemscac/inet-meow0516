4239d672df902d019a50c7bee0f6430b83ffc8e5
meow0516 <meow0516@gmail.com>
2022-08-22 12:15:13 +0800

    fix: import HomePageSidebar

--- src/components/pages/SearchPage.vue
+++ src/components/pages/SearchPage.vue
@@ -8,6 +8,7 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import { useRoute } from 'vue-router';
+import HomePageSidebar from './HomePageSidebar.vue';
 import SearchPageArticleList from './SearchPageArticleList.vue';
 
 const route = useRoute();
